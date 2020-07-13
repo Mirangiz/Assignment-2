0245c7e998374456f8d901ec015f2834b73cae01
Mirangiz <68215895+Mirangiz@users.noreply.github.com>
2020-07-13 10:00:08 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
+cachemean <- function(x, ...) {
+       m <- x$getmean()
+       if(!is.null(m)) {
+             message("getting cached data")
+           return(m)
+         }
+      data <- x$get()
+       m <- mean(data, ...)
+       x$setmean(m)
+       m 
+   }
+makeCasheMatrix <- function(x=matrix()) {
+     inv <- NULL
+    set <- function(y){
+         x<<-y
+         inv<<-NULL
+       }
+     get<-function() {x}
+     setInverse <- function(inverse) {inv<<-inverse}
+     getInverse <- function() {inv}
+     list(set=set,get=get, setInverse=setInverse,getInverse=getInverse)
+   }
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+       inv <-x$getInverse()
+      if(!is.null(inv)) {
+             message("getting catched data")
+             return(inv)
+         }
+       mat <- x$get()
+       inv <- solve(mat,...)
+     x$setInverse(inv)
+   inv
+   }
+   > matrix1 <- makeCasheMatrix(matrix(3:6, nrow=2, ncol=2))
+> matrix1$get()
+     [,1] [,2]
+[1,]    3    5
+[2,]    4    6
+> matrix1$getInverse()
+NULL
+> cacheSolve(matrix1)
+     [,1] [,2]
+[1,]   -3  2.5
+[2,]    2 -1.5
